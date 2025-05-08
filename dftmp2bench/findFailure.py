dd6e235313ac75e839d184e678be3c8bc98efd92
Magalie Brandes <brandes@master.cluster>
2025-05-08 15:24:06 +0200

    start to find fails

---
+++ dftmp2bench/findFailure.py
@@ -0,0 +1,14 @@
+# TODO:
+# read a csv file from the output step and read the termination and decide to rerun the job (or not?)
+#
+
+
+
+def find_faulty(csv: str) -> n: int:
+    """ 
+    returns n (number of failures)
+    """
+
+    pass
+
+
